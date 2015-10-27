8cc6094778d7360c1c7645fe345825626657464a
Lokesh <lokesh.gahlot@wellsfargo.com>
2015-10-27 09:40:34 -0700

    Rest-default sample added. Changes to be committed: modified:   src/index.jade new file:   src/rest-default.babelify.js

---
+++ src/rest-default.babelify.js
@@ -0,0 +1,30 @@
+;(function() {
+  'use strict';
+  // ----------------------------
+  // Default
+  // ----------------------------
+
+  console.log('************** Default *****************');
+  function f1(x, y=12) {
+    // y is 12 if not passed (or passed as undefined)
+    return x + y;
+  }
+  console.log(f1(3));
+
+  // ----------------------------
+  // Rest
+  // ----------------------------
+  console.log('************** Rest *****************');
+  function f2(x, ...y) {
+    // y is an Array
+    return x * y.length;
+  }
+  console.log(f2(3, "hello", true));
+
+  function f3(x, y, z) {
+    return x + y + z;
+  }
+  // Pass each elem of array as argument
+  console.log(f3(...[1,2,3]));
+
+})();
