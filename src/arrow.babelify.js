6f95c77ab1df69368f800877bf8994ab2fc01ac1
Lokesh <lokesh.gahlot@wellsfargo.com>
2015-10-27 08:26:08 -0700

    Arrow function added new file:   src/arrow.babelify.js new file:   src/index.jade

---
+++ src/arrow.babelify.js
@@ -0,0 +1,9 @@
+;(function() {
+  var evens = [2, 4, 6, 8];
+
+  var odds = evens.map(v => v + 1);
+  var nums = evens.map((v, i) => v + i);
+  var pairs = evens.map(v => ({even: v, odd: v + 1}));
+  var k = c=>(p + 1);
+
+})();
