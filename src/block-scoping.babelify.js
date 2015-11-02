1c7f6acc1ae779d52b56c8856c0a693f74b71aac
Lokesh <lokesh.gahlot@wellsfargo.com>
2015-11-01 19:58:02 -0800

    block-scope sample added new file:   src/block-scoping.babelify.js modified:   src/index.jade

---
+++ src/block-scoping.babelify.js
@@ -0,0 +1,9 @@
+console.log('********** Block scope ************');
+for (let i = 0; i <= 10; i++) {
+  console.log('i = ', i);
+}
+console.log('..... i ', i); {
+  let a = 'test';
+}
+
+console.log('a');
